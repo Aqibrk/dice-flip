29fb26378f6ca35b6b884e53bc2aabc8ec5bd17b
Aqibrk <99348346+Aqibrk@users.noreply.github.com>
2022-03-31 10:30:24 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,18 @@
+function change(){
+    const play1 = Math.floor(Math.random() * 6) +1;
+    const play1dice ="images/dice"+play1+ ".png";
+    document.getElementById('check1').setAttribute("src",play1dice);
+
+    const play2 = Math.floor(Math.random() * 6) +1;
+    const play2dice ="images/dice"+play2+ ".png";
+    document.getElementById('check2').setAttribute("src",play2dice);
+
+
+if(play1 > play2)
+   { document.querySelector('h1').innerHTML="play1 won :)" 
+}else if(play2 < play1)
+    { document.querySelector('h1').innerHTML="play2 won :)";   
+}else
+     {document.querySelector('h1').innerHTML="Draw";    }
+
+}
